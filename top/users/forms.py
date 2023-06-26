82020248eae4e864d4a852227f11de77b3e32d09
Apollo2905 <polina.shashilo@mail.ru>
2023-06-26 19:37:12 +0300

    Создание детальной страницы товара

--- top/users/forms.py
+++ top/users/forms.py
@@ -1,35 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from .bulma_mixin import BulmaMixin
 
 
-class SignInForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'input', 'placeholder': 'Введите никнейм'}
-    ))
-    password = forms.CharField(widget=forms.PasswordInput(
-        attrs={'class': 'input', 'placeholder': 'Введите пароль'}
-    ))
+class SignInForm(BulmaMixin, AuthenticationForm):
+    username = forms.CharField(label='Напишите никнейм')
+    password = forms.CharField(widget=forms.PasswordInput(), label='Напишите пароль')
 
     class Meta:
         model = User
         fields = ['username', 'password']
 
 
-class SignUpForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'input', 'placeholder': 'Придумайте никнейм'}
-    ))
-    password1 = forms.CharField(widget=forms.PasswordInput(
-        attrs={'class': 'input', 'placeholder': 'Придуймайте пароль'}
-    ))
-    password2 = forms.CharField(widget=forms.PasswordInput(
-        attrs={'class': 'input', 'placeholder': 'Повторите пароль'}
-    ))
-
-    email = forms.CharField(widget=forms.EmailInput(
-        attrs={'class': 'input', 'placeholder': 'Введите адрес почты'}
-    ))
+class SignUpForm(BulmaMixin, UserCreationForm):
+    username = forms.CharField(label='Придумайте никнейм')
+    password1 = forms.CharField(widget=forms.PasswordInput(), label='Придумайте пароль')
+    password2 = forms.CharField(widget=forms.PasswordInput(), label='Повторите пароль')
+    email = forms.CharField(widget=forms.EmailInput(), label='Напишите адрес почты')
 
     class Meta:
         model = User
